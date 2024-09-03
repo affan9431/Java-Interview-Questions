62e5201a2c52c9153917e2213ba5866783e6b365
affan9431 <123025121+affan9431@users.noreply.github.com>
2024-09-03 14:37:21 +0530

    Update character_frequnecy.java

--- character_frequnecy.java
+++ character_frequnecy.java
@@ -1,38 +1,73 @@
 import java.util.Scanner;
 
-public class character_frequnecy {
-    // stringFollowedByNumber() is a function that take string like abbbccc and convert to a1b3c3
+public class CharacterFrequency {
+
+    /***
+     * Question: Expand Characters in a String
+     * Owner name: Affan Sayeed
+     *  stringFollowedByNumber() is a  method which take input like abbbccc and convert into ab3c3.
+     * Date: 3-9-2024
+     */
+
+    // stringFollowedByNumber() is a function that takes a string like abbbccc and
+    // converts it to a1b3c3
     public static void stringFollowedByNumber() {
         String input;
-        System.out.println("Enter the chracter like(abbbccc):: ");
+        System.out.println(Constants.ENTER_FREQ_STRING);
         Scanner sc = new Scanner(System.in);
         input = sc.next();
-        sc.close();
 
-        StringBuilder output = new StringBuilder();
+        String output = "";
         int i = 0;
 
-        // check if input conatain any uppercase or not
-        if (input.contains(input.toUpperCase())) {
-            System.out.println("Please enter English letter in lowercase");
+        // check if input contains any uppercase letters
+        if (!input.equals(input.toLowerCase())) {
+            System.out.println(Constants.LOWER_WARN);
         } else {
 
             while (i < input.length()) {
                 char character = input.charAt(i);
                 int count = 0;
-                // if i <  input.length() and input conatain any repeated character then increament count and i; 
+
+                // count occurrences of the current character
                 while (i < input.length() && input.charAt(i) == character) {
                     count++;
                     i++;
                 }
-                // append the character and count to output string builder.
-                output.append(character).append(count);
+
+                // append the character and its count to the output string
+                output += character;
+                output += count;
             }
             System.out.println(output);
         }
     }
 
     public static void main(String[] args) {
-        stringFollowedByNumber();
+        Scanner sc = new Scanner(System.in);
+        int key;
+        boolean continueProgram = true;
+
+        while (continueProgram) {
+            System.out.println(Constants.OPTION_ONE);
+            System.out.println(Constants.OPTION_TWO);
+            System.out.println(Constants.ENTER_HERE);
+            key = sc.nextInt();
+
+            switch (key) {
+                case 1:
+                    stringFollowedByNumber();
+                    break;
+
+                case 2:
+                    continueProgram = false;
+                    break;
+
+                default:
+                    System.out.println(Constants.DEFAULT);
+                    break;
+            }
+        }
+        sc.close();
     }
 }
